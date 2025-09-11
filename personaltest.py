76fa20bd6b55c87ec01a17fbcd1eb7191a190ee5
Lolo <59346039+LoloGitz@users.noreply.github.com>
2025-09-11 16:16:10 +0000

    tests

--- personaltest.py
+++ personaltest.py
@@ -3,7 +3,7 @@ import math
 import os
 import time
 
-load_iterations = 1
+load_iterations = 4
 load_size = 50
 
 tiles_loaded = 0
@@ -18,18 +18,26 @@ def clear_terminal():
         os.system("clear")
 
 def updateLoading():
-    clear_terminal()
-
+    global tiles_loaded
     global tiles_to_load
-    global tiles_loaded    
-    tiles_loaded += 1
-    print("loading map... " + str(tiles_loaded / tiles_to_load * 1000) + "%")
+
+    ratio = round(tiles_loaded / tiles_to_load * 100) / 100
+    ratio_bar = math.floor(ratio * 10)  
+
+    clear_terminal()
+    print(
+        "loading map... " 
+        + str(round(ratio * 100))
+        + "%\n-[ " + ("■" * ratio_bar)
+        + ("□" * (10 - ratio_bar))
+        + " ]-"
+    )
+    
 
 def generateNoise(x: int, y: int, iterations: int, batch: dict = None) -> dict:
+    global tiles_loaded
     result = {}
-
-    lowest = 1
-    highest = 0
+    lowest, highest = 1, 0
 
     for y_i in range(y):
         result[y_i] = {}
@@ -41,23 +49,23 @@ def generateNoise(x: int, y: int, iterations: int, batch: dict = None) -> dict:
             else:
                 new_value = 0
                 neighbors = 0
-                
 
                 for i in range(9):
                     xx = x_i + ((i % 3) - 1)
                     yy = y_i + (math.floor(i / 3) - 1)
                     
                     if (xx >= 0 and xx <= (x - 1) and yy >= 0 and yy <= (y - 1)):
-                         # print(xx, yy)
                         new_value += batch[yy][xx]
                         neighbors += 1
                     
                 weight = new_value / neighbors
 
-            lowest = min(lowest, weight)
-            highest = max(highest, weight)
+            lowest, highest = min(lowest, weight), max(highest, weight)
             result[y_i][x_i] = weight
-            updateLoading()
+
+            tiles_loaded += 1
+            if (tiles_loaded % math.floor(tiles_to_load / 100) == 0):
+                updateLoading()
                     
     if (iterations > 1):
         return generateNoise(x, y, iterations - 1, result)
@@ -72,7 +80,9 @@ def generateNoise(x: int, y: int, iterations: int, batch: dict = None) -> dict:
 
         return filt
     
+tick = time.time()
 noise = generateNoise(load_size, load_size, load_iterations)
+print(time.time() - tick)
 
 # for y in noise:
 #     y_values = noise[y]
@@ -80,15 +90,19 @@ noise = generateNoise(load_size, load_size, load_iterations)
 #         weight = y_values[x]
 #         print(x, y, weight) -- debug
 
-for y in noise:
-    y_values = noise[y]
-    print()
-    for x in y_values:
-        weight = y_values[x]
-        if(weight >= 0.7):
-            print('🟩',end='')
-        elif (weight >= 0.55):
-            print('🟨',end='')
-        else:
-            print('🟦',end='')
-        
+while True:
+    clear_terminal()
+
+    for y in noise:
+        y_values = noise[y]
+        print()
+        for x in y_values:
+            weight = y_values[x]
+            if(weight >= 0.7):
+                print('🟩',end='')
+            elif (weight >= 0.55):
+                print('🟨',end='')
+            else:
+                print('🟦',end='')
+
+        print('',end='\r')
